3819b64806967f947a0687bb15616b99e2bd0562
cHI11F <nazar.vata@dspu.edu.ua>
2023-11-06 16:52:29 +0200

    initial commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR16/PR16.cpp"  
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+    TEST_CLASS(UnitTest2)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            // Arrange
+            double x = 0.5;
+            double eps = 0.0001;
+            int n = 0;
+            double s = 0;
+            double expected = sin(x) / x;
+
+            // Act
+            double actual = S(x, eps, n, s);
+
+            // Assert
+            Assert::AreEqual(expected, actual, eps);
+        }
+    };
+}
